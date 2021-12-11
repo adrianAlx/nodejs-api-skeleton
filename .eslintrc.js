ffab7b48d1b6765310e67a802677862aca4b3220
Alex Martin <alextravc.342@gmail.com>
2021-12-11 18:17:57 -0500

    [ADD]: Initial project structure

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+  },
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Express API Bootstrap (base / project starter)
+
+This is a repository intended to serve as a starting point if you want to bootstrap a express API project.
